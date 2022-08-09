3dcf1cda0e0a2705fd4c634313213921bac11193
star-min <zxzx00262@gmail.com>
2022-08-09 18:25:19 +0900

    0809
    spring boot update

---
+++ spring_boot/mychat/src/main/java/com/company/mychat/handler/WebsocketHandler.java
@@ -0,0 +1,13 @@
+package com.company.mychat.handler;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class WebsocketHandler extends TextWebSocketHandler {
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, )
+}
